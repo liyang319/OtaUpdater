43af88cca0a8602f5229489b36005f41892b4ca8
liyang <79628285@qq.com>
2024-06-14 22:50:23 +0800

    good script

--- app/config/update_replace.sh
+++ app/config/update_replace.sh
@@ -4,54 +4,46 @@ srcPath="/home/app/ota_save"
 desPath="/home/app"
 AppName="ControlBox"
 UpdaterName="OtaUpdater"
-# fileList=("ControlBox" "OtaUpdater" "ControlBox.ini")
 fileList=("ControlBox")
+
+# 创建日志文件
+logFile="/home/app/ota_log.log"
+touch $logFile
 
 # 检查源文件路径是否存在
 if [ -d "$srcPath" ]; then
-  # 检查目标文件中的$AppName文件是否存在
   if [ -f "$desPath/$AppName" ]; then
-    echo "$AppName 文件存在，杀死$AppName进程"
+    echo "$AppName 文件存在，杀死$AppName进程" >> $logFile
     pkill -f $AppName
-    # pkill -f $UpdaterName
-    echo "Waiting..."
+    echo "Waiting..." >> $logFile
     sleep 1
+    echo "关闭看门狗..." >> $logFile
+    echo V > /dev/watchdog
   fi
 
-# 源文件路径存在，开始拷贝操作
+  # 源文件路径存在，开始拷贝操作
   for file in "${fileList[@]}"; do
     srcFile="$srcPath/$file"
     if [ -f "$srcFile" ]; then
-      # 如果是fileList中的文件，则进行拷贝并打印文件名
-      cp -f "$srcFile" "$desPath"
-      echo "拷贝文件: $file"
+      mv -f "$srcFile" "$desPath"
+      echo "拷贝文件: $file" >> $logFile
     else
-      echo "$srcFile 不存在"
+      echo "$srcFile 不存在" >> $logFile
     fi
   done
 
-  # 检查拷贝是否成功
   if [ $? -eq 0 ]; then
-    echo "文件拷贝成功"
+    echo "文件拷贝成功" >> $logFile
 
-    # 检查目标文件中的$AppName文件是否存在
     if [ -f "$desPath/$AppName" ]; then
-      echo "$AppName 文件存在，启动$AppName"
-
-      # 给文件权限777
-      # chmod 777 "$desPath/$UpdaterName"
+      echo "$AppName 文件存在，启动$AppName" >> $logFile
       chmod 777 "$desPath/$AppName"
-      # "$desPath/$AppName" &
-      # 等待1秒
-      # sleep 3
-      # echo "启动$UpdaterName"
-      # "$desPath/$UpdaterName" &
     else
-      echo "$AppName 文件不存在"
+      echo "$AppName 文件不存在" >> $logFile
     fi
   else
-    echo "文件拷贝失败"
+    echo "文件拷贝失败" >> $logFile
   fi
 else
-  echo "源文件路径不存在"
+  echo "源文件路径不存在" >> $logFile
 fi
